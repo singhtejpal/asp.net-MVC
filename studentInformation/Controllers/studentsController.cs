617826d527ce26829f6b68a96b818b356750f450
tejpal singh <singhtej955@gmail.com>
2017-01-23 17:53:28 -0500

    initial commit

---
+++ studentInformation/Controllers/studentsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using studentInformation.Models;
+
+namespace studentInformation.Controllers
+{
+    public class studentsController : Controller
+    {
+        // GET: students
+        public ActionResult studentInfo()
+        {
+            students s1 = new students();
+            s1.name = "tejpal";
+            s1.student_number = "n01194263";
+            s1.program_name = "Web Development";
+
+            ViewBag.title = "student info";
+            return View(s1);
+        }
+    }
+}
---
+++ studentInformation/Models/students.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace studentInformation.Models
+{
+    public class students
+    {
+        public string name { get; set; }
+        public string student_number { get; set; }
+        public string program_name { get; set; }
+
+    }
+}
